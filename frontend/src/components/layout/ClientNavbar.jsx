e93b9ca3637a9614973840df9b0daef8f44e6f8d
Ilyes chniti <elyeschniti85@gmail.com>
2026-07-08 12:37:54 +0100

    Ameliorer navbar client et catalogue

--- frontend/src/components/layout/ClientNavbar.jsx
+++ frontend/src/components/layout/ClientNavbar.jsx
@@ -6,21 +6,48 @@ const ClientNavbar = () => {
 
   return (
     <nav className="client-navbar">
-      <div className="client-navbar-logo">
+      <Link to="/client/catalogue" className="client-navbar-logo">
         Gestion Matériel
-      </div>
+      </Link>
 
       <div className="client-navbar-links">
         <Link to="/client/catalogue">Catalogue</Link>
         <Link to="/client/mes-emprunts">Mes emprunts</Link>
-        <Link to="/client/notifications">Notifications</Link>
-        <Link to="/client/historique">Historique</Link>
-        <Link to="/client/profil">Profil</Link>
         <Link to="/client/panier">Panier</Link>
-        <Link to="/client/ajouter-materiel">Ajouter matériel</Link>
-        <Link to="/client/mes-materiels">Mes matériels</Link>
-        <Link to="/client/demandes-recues">Demandes reçues</Link>
-        <Link to="/client/demander-societe">Créer société</Link>
+        <Link to="/client/notifications">Notifications</Link>
+
+        <div className="client-navbar-dropdown">
+          <button type="button" className="client-navbar-dropdown-button">
+            Mes annonces ▾
+          </button>
+
+          <div className="client-navbar-dropdown-menu">
+            <Link to="/client/ajouter-materiel">Ajouter matériel</Link>
+            <Link to="/client/mes-materiels">Mes matériels</Link>
+            <Link to="/client/demandes-recues">Demandes reçues</Link>
+          </div>
+        </div>
+
+        <div className="client-navbar-dropdown">
+          <button type="button" className="client-navbar-dropdown-button">
+            Société ▾
+          </button>
+
+          <div className="client-navbar-dropdown-menu">
+            <Link to="/client/demander-societe">Créer société</Link>
+          </div>
+        </div>
+
+        <div className="client-navbar-dropdown">
+          <button type="button" className="client-navbar-dropdown-button">
+            Mon compte ▾
+          </button>
+
+          <div className="client-navbar-dropdown-menu">
+            <Link to="/client/profil">Profil</Link>
+            <Link to="/client/historique">Historique</Link>
+          </div>
+        </div>
       </div>
 
       <div className="client-navbar-user">
@@ -28,7 +55,9 @@ const ClientNavbar = () => {
           {user?.prenom} {user?.nom}
         </span>
 
-        <button onClick={logout}>Déconnexion</button>
+        <button type="button" onClick={logout}>
+          Déconnexion
+        </button>
       </div>
     </nav>
   );
